0f6bc78519c3ebcadfed87b4c4ce61f0c1aafed8
Idyllist <100647714+idyllist37@users.noreply.github.com>
2023-09-09 13:52:33 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Idyllist</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} Idyllist</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
